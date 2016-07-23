8be8941d0793f79092aa76a831d8b21c5dd53b9e
Björn Büttner <Idrinth@users.noreply.github.com>
2016-07-23 02:52:14 +0200

    fixes #4 overlooked v n version creation json

--- Arzynik/Task/Release.php
+++ Arzynik/Task/Release.php
@@ -3,7 +3,7 @@ use Arzynik\Service\Github;
 class Release {
     public function run($repository,$version,$tickets,$zipFile) {
         $curl = new Github();
-        $data = $curl->send('repos/' . $repository . '/releases','{"tag_name": "' . $version . '","name": "v' . $version . '","body": "Automatic Release\n Fixes to #' . implode(', #',$tickets) . '"}','post','application/json');
+        $data = $curl->send('repos/' . $repository . '/releases','{"tag_name": "' . $version . '","name": "' . $version . '","body": "Automatic Release\n Fixes to #' . implode(', #',$tickets) . '"}','post','application/json');
         if(isset($data->assets_url) && $data->assets_url) {
             $curl->send($data->assets_url . '?name=' . explode('/',$repository)[1] . '.zip',file_get_contents($zipFile),'post','application/zip');
             if($data->browser_download_url) {
